a0d5c1a96d57a8f09401c80061d4def0e61cb3f5
Israel20211 <85128009+Israel20211@users.noreply.github.com>
2021-10-04 22:00:24 -0500

    Mejoras al botón menú

--- menu.js
+++ menu.js
@@ -1,24 +1,27 @@
 $(document).ready(main); //Indica la funcion principal
 var contador = 1;
-var button = document.getElementById("btnMenu");
+//var button = document.getElementById("btnMenu");
 function main(){
-	/*	$('.Menu_barras').click(function(){ //Función OnClick
+	$('.Menu_barras').click(function(){ //Función OnClick
 		if(contador == 1){
 			$('nav').animate({
 				left:'0' //Hace que Nav aparezca 
 			});
 		contador = 0;
+		document.getElementById('img1').style.display = 'none';
+		document.getElementById('img2').style.display = 'none';
 	}else{
 		$('nav').animate({
 			left:'-120%' //Vuelve a ocultar nav
 		});
 		contador = 1;
+		document.getElementById('img1').style.display = 'none';
+		document.getElementById('img2').style.display = 'none';
 	}
 		
 	});
-	*/
 }
-button.onclick=function(){
+/*button.onclick=function(){
 	if(contador == 1){			
 			$('nav').animate({
 				left:'0' //Hace que Nav aparezca 
@@ -35,4 +38,4 @@ button.onclick=function(){
 	
 		contador = 1;
 	}
-}
+}*/
